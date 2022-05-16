34671802840e7e6a1625bf80f1a9c5c748577222
das-rob3rt <rob3rt.233@gmail.com>
2022-05-16 14:53:18 +0000

    Update from https://github.com/FreeBSD-Ask/FreeBSD-Ask/commit/338483187280736f1bef6da8dd61592f8146b3f6

--- docs/.vuepress/configs/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @file configs/index.ts
- * @brief Configuration details.
- * @copyright Copyright (c) 2022 The FreeBSD Ask Authors. All rights reserved.
- */
-
-export { sideBarConfig } from "./sidebar";
