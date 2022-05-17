654efbc0ee05c13dc1600558ab11d315b643ea15
DasRob3rt <rob3rt.233@gmail.com>
2022-05-17 13:55:52 +0800

    [YARN] Upgrade vuepress 2.0.0-beta.45

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,32 +4,26 @@
  * @copyright Copyright (c) 2022 The FreeBSD Ask Authors. All rights reserved.
  */
 
-import type { DefaultThemeOptions } from "vuepress";
+import { docsearchPlugin } from "@vuepress/plugin-docsearch";
+import { defaultTheme } from "vuepress";
 import { defineUserConfig } from "vuepress";
 import { sideBarConfig } from "./configs";
 
-export default defineUserConfig<DefaultThemeOptions>({
-  // site config
+export default defineUserConfig({
   lang: "zh-CN",
   title: "FreeBSD 从入门到跑路",
   description: "FreeBSD 从入门到跑路",
 
-  // theme and its config
-  theme: "@vuepress/theme-default",
-  themeConfig: {
+  theme: defaultTheme({
     logo: "https://vuejs.org/images/logo.png",
     sidebar: sideBarConfig,
-  },
+  }),
 
-  // plugins and their config
   plugins: [
-    [
-      "@vuepress/plugin-docsearch",
-      {
-        appId: "4KCP05N90H",
-        apiKey: "938386dc0789ce87b047a5a589001913",
-        indexName: "freebsdcn",
-      },
-    ],
+    docsearchPlugin({
+      appId: "4KCP05N90H",
+      apiKey: "938386dc0789ce87b047a5a589001913",
+      indexName: "freebsdcn",
+    }),
   ],
 });
