93f8f74997246aa86ea16e914d2e9c5c44d6f393
Das Rob3rt <rob3rt.233@gmail.com>
2022-06-04 16:02:05 +0800

    [CFG] Add Google Analytics plugin

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -4,6 +4,7 @@
  * @copyright Copyright (c) 2022 FreeBSD Chinese Community. All rights reserved.
  */
 
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 import { defaultTheme, defineUserConfig } from "vuepress";
 import { fullTextSearchPlugin } from "vuepress2-plugin-full-text-search";
 import { sideBarConfig } from "./configs";
@@ -14,9 +15,14 @@ export default defineUserConfig({
   description: "FreeBSD 从入门到跑路",
 
   theme: defaultTheme({
-    logo: "https://book.freebsdcn.org/favicon.ico",
+    logo: "https://book.bsdcn.org/favicon.ico",
     sidebar: sideBarConfig,
   }),
 
-  plugins: [[fullTextSearchPlugin]],
+  plugins: [
+    fullTextSearchPlugin,
+    googleAnalyticsPlugin({
+      id: "G-31WQ8W3FF6",
+    }),
+  ],
 });
