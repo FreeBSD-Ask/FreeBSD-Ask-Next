026881be3a0b2e87025ce27e864de5a273d2552f
ykla <yklaxds@gmail.com>
2022-10-19 03:41:34 +0800

    提交测试

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,7 @@ import { defineUserConfig } from '@vuepress/cli'
 import { sitemapPlugin } from 'vuepress-plugin-sitemap2'
 import { seoPlugin } from 'vuepress-plugin-seo2'
 import { defaultTheme } from '@vuepress/theme-default'
-import { fullTextSearchPlugin  } from 'vuepress-plugin-full-text-search2'
+import { docsearchPlugin } from '@vuepress/plugin-docsearch'
 
 import {sideBarConfig} from './configs'
 
@@ -33,13 +33,51 @@ export default defineUserConfig({
     googleAnalyticsPlugin({
       id: 'G-31WQ8W3FF6',
     }),
-   fullTextSearchPlugin({
-      locales: {
-        '/': {
-          placeholder: 'Search',
-        },
-        '/zh/': {
-          placeholder: '搜索',
+   docsearchPlugin({
+      appId: 'MZ41VSDUQ5',
+      apiKey: '04a9684aba6690eb58ad1fb349a056b8',
+      indexName: 'book-bsdcn',
+	  locales: {
+        "/": {
+          placeholder: "搜索",
+		   translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除搜索条件",
+                resetButtonAriaLabel: "清除搜索条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "无搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试搜索",
+                reportMissingResultsText: "没有找到应有的搜索结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },	  
+	        },
+          },
         },
       },
     }),
