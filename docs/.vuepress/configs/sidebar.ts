5360690060b3acab6a53f452d488827dbf920007
ykla <yklaxds@gmail.com>
2023-06-15 21:26:48 +0800

    Update sidebar.ts

--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -380,7 +380,8 @@ export const sideBarConfig: SidebarConfig = [
 		'/di-30-zhang-linux-jian-rong-ceng/di-30.2-jie-linux-jian-rong-ceng-ji-yu-centos-yuan-sheng.md',
 		'/di-30-zhang-linux-jian-rong-ceng/di-30.3-jie-linux-jian-rong-ceng-ji-yu-ubuntudebian.md',
 		'/di-30-zhang-linux-jian-rong-ceng/di-30.4-jie-linux-jian-rong-ceng-ji-yu-archlinux.md',
-		'/di-30-zhang-linux-jian-rong-ceng/di-30.5-jie-gao-ji-jiao-cheng-linux-jian-rong-ceng-yu-jail.md',
+		'/di-30-zhang-linux-jian-rong-ceng/di-30.5-jie-linux-jian-rong-ceng-ji-yu-archlinuxpacman.md',
+	    	'/di-30-zhang-linux-jian-rong-ceng/di-30.6-jie-gao-ji-jiao-cheng-linux-jian-rong-ceng-yu-jail.md',
     ],
   },
  {
