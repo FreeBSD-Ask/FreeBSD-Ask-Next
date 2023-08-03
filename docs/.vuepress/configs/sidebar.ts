fec84ffb9265b6f936b49463137afccc97472a57
ykla <yklaxds@gmail.com>
2023-08-03 18:13:17 +0800

    Update sidebar.ts

--- docs/.vuepress/configs/sidebar.ts
+++ docs/.vuepress/configs/sidebar.ts
@@ -25,6 +25,7 @@ export const sideBarConfig: SidebarConfig = [
 		'/di-0-zhang-freebsd-zhong-wen-she-qu/di-0.4-jie-freebsd-fa-hang-shuo-ming.md',
 	    	'/di-0-zhang-freebsd-zhong-wen-she-qu/di-0.5-jie-freebsd-ji-du-bao-gao.md',
 	    	'/di-0-zhang-freebsd-zhong-wen-she-qu/di-0.6-jie-freebsd-kai-fa-ji-hua.md',
+	    	'/di-0-zhang-freebsd-zhong-wen-she-qu/di-0.7-jie-freebsd-an-quan-gong-gao.md',
     ],
   },
   {
